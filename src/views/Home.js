e9a262cc1115186bad1cfacfba338633d87bf4a2
Divyansh Dubey <divyanshdubey938@gmail.com>
2023-10-07 00:48:03 +0530

    basic boilerplate

---
+++ src/views/Home.js
@@ -0,0 +1,18 @@
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  CardText,
+  CardLink,
+} from "reactstrap";
+
+const Home = () => {
+  return (
+    <div>
+     Hello
+    </div>
+  );
+};
+
+export default Home;
